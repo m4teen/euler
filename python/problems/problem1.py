89bae0a549d50b0086387e81b9c860ac1bf54217
m4teen <104398485+m4teen@users.noreply.github.com>
2025-07-03 21:12:23 +0100

    Create problem1.py

---
+++ python/problems/problem1.py
@@ -0,0 +1,18 @@
+#%%
+
+"""""
+
+Problem: 
+
+1. Maths theory: dealing with natural numbers and multiples, thus there may be division involved
+
+2. Examples of theory: find lcm, find gcd, find coprime pairs, sum multiples, find modulo p numbers 
+
+3. We are essentially dividing the numbers in the list to check if we get a 0 residue in the mod operation 
+for 3 or 5, which is fine. 
+
+Caution 1: But we want to consider numbers strictly below n (off by one error)
+Caution 2: Don't want to double add numbers divisble by both 3 and 5 (inclusion-exclusion)
+
+
+"""""
