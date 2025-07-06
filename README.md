5fd70c8e372c4ca489e7836863a60e1a0d35e175
m4teen <104398485+m4teen@users.noreply.github.com>
2025-07-06 22:16:06 +0100

    p2 solved

---
+++ README.md
@@ -0,0 +1,15 @@
+# Project Euler Solutions
+
+My solutions for [Project Euler](https://projecteuler.net/) – a
+collection of challenging mathematical and computational problems that
+require more than just mathematical insights to solve.
+
+This repository contains my personal journey through the problems,
+along with clean and self-documenting solutions in code.
+
+## Progress Tracker
+
+| Problem | Status | Code           |
+| ------- | ------ |----------------|
+| 1       | Done   | [p1.py](p1.py) |
+
---
+++ p2.py
@@ -0,0 +1,29 @@
+def f(n):
+    if n == 0:
+        return 0
+    elif n ==1:
+        return 1
+    elif n > 1:
+        return f(n-1)+f(n-2)
+    else:
+        print('Negative numbers are forbidden.')
+
+def fib_summer(m):
+    fibs = []
+    even_fibs = []
+    n = 0
+    while f(n) <= m:
+        fibs.append(f(n))
+        n += 1
+
+    for i in fibs:
+        if i % 2 == 0:
+            even_fibs.append(i)
+
+    total_even_fibs = sum(even_fibs)
+
+    return total_even_fibs
+
+
+print(fib_summer(4000000))
+
